601064a29d2a80b1b415fdf4e2d844a2b53cad0e
aymendamak <aymen.damak@yahoo.fr>
2024-12-13 22:32:46 +0100

    Add Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { ButtonPrimary } from "./Button";
+
 const sitemap = [
   {
     label: "Home",
@@ -47,7 +49,78 @@ const socials = [
 ];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="section">
+      <div className="container">
+        <div className="lg:grid lg:grid-cols-2">
+          <div className="mb-10">
+            <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
+              Let&apos;s work together today!
+            </h2>
+
+            <ButtonPrimary
+              href="mailto:aymendamak2@gmail.com"
+              label="Start Project"
+              icon="chevron_right"
+            />
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 lg:pl-20">
+            <div>
+              <p className="mb-2">Sitemap</p>
+              <ul>
+                {sitemap.map(({ label, href }, key) => (
+                  <li key={key}>
+                    <a
+                      href={href}
+                      className="block text-sm text-zinc-400 py-1 
+                      transition-colors hover:text-zinc-200"
+                    >
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div>
+              <p className="mb-2">Socials</p>
+              <ul>
+                {socials.map(({ label, href }, key) => (
+                  <li key={key}>
+                    <a
+                      href={href}
+                      className="block text-sm text-zinc-400 py-1 
+                      transition-colors hover:text-zinc-200"
+                      target="_blank"
+                    >
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-between pt-10 mb-8">
+          <a href="" className="">
+            <img
+              src="/images/logo.svg"
+              width={40}
+              height={40}
+              alt="Logo"
+              className=""
+            />
+          </a>
+
+          <p className="text-zinc-500 text-sm">
+            &copy; 2024{" "}
+            <span className="text-zinc-200">Code With Aymen Damak</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
