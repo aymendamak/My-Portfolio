a229100fe4cd66f8a3d2dfc18aadedeb60dbf640
aymendamak <aymen.damak@yahoo.fr>
2024-12-14 11:46:37 +0100

    verify variable before access params

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,10 +9,13 @@ const NavBar = ({ navOpen }: Props) => {
   const activeBox = useRef();
 
   const initActiveBox = () => {
-    activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
-    activeBox.current.style.left = lastActiveLink.current.offsetLeft + "px";
-    activeBox.current.style.width = lastActiveLink.current.offsetWidth + "px";
-    activeBox.current.style.height = lastActiveLink.current.offsetHeight + "px";
+    if (activeBox && activeBox.current) {
+      activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
+      activeBox.current.style.left = lastActiveLink.current.offsetLeft + "px";
+      activeBox.current.style.width = lastActiveLink.current.offsetWidth + "px";
+      activeBox.current.style.height =
+        lastActiveLink.current.offsetHeight + "px";
+    }
   };
 
   useEffect(initActiveBox, []);
@@ -23,10 +26,12 @@ const NavBar = ({ navOpen }: Props) => {
     event.target.classList.add("active");
     lastActiveLink.current = event?.target;
 
-    activeBox.current.style.top = event.target.offsetTop + "px";
-    activeBox.current.style.left = event.target.offsetLeft + "px";
-    activeBox.current.style.width = event.target.offsetWidth + "px";
-    activeBox.current.style.height = event.target.offsetHeight + "px";
+    if (activeBox && activeBox.current) {
+      activeBox.current.style.top = event.target.offsetTop + "px";
+      activeBox.current.style.left = event.target.offsetLeft + "px";
+      activeBox.current.style.width = event.target.offsetWidth + "px";
+      activeBox.current.style.height = event.target.offsetHeight + "px";
+    }
   };
 
   const navItems = [
