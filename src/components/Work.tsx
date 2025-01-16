e8b44abb650ef3cc134fcc26324d21122e856b39
aymendamak <aymen.damak@yahoo.fr>
2025-01-16 12:10:30 +0100

    Update my work highlights

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -7,36 +7,6 @@ const works = [
     tags: ["ReactJS", "Vite", "TypeScript", "TailWindCSS"],
     projectLink: "https://todo-app-rho-opal-82.vercel.app/",
   },
-  {
-    imgSrc: "/images/project-2.jpg",
-    title: "Free stock photo app",
-    tags: ["API", "SPA"],
-    projectLink: "https://pixstock-official.vercel.app/",
-  },
-  {
-    imgSrc: "/images/project-3.jpg",
-    title: "Recipe app",
-    tags: ["Development", "API"],
-    projectLink: "",
-  },
-  {
-    imgSrc: "/images/project-4.jpg",
-    title: "Real state website",
-    tags: ["Web-design", "Development"],
-    projectLink: "https://github.com/codewithsadee-org/wealthome",
-  },
-  {
-    imgSrc: "/images/project-5.jpg",
-    title: "eCommerce website",
-    tags: ["eCommerce", "Development"],
-    projectLink: "https://github.com/codewithsadee/anon-ecommerce-website",
-  },
-  {
-    imgSrc: "/images/project-6.jpg",
-    title: "vCard Personal portfolio",
-    tags: ["Web-design", "Development"],
-    projectLink: "https://github.com/codewithsadee/vcard-personal-portfolio",
-  },
 ];
 
 const Work = () => {
